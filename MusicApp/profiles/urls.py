b1892c9ac50feaae66d29b57b000815f7b7644ab
Zhenya Todorova <73773939+zhenyatodorova0@users.noreply.github.com>
2026-03-17 08:37:24 +0200

    Delete MusicApp directory

--- MusicApp/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from profiles import views
-
-app_name = "profiles"
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('details/', views.ProfileDetailView.as_view(), name='details'),
-    path('delete/', views.ProfileDeleteView.as_view(), name='delete'),
-]
--- MusicApp/profiles/util.py
+++
@@ -1,6 +0,0 @@
-#помощна функция
-
-from profiles.models import Profile
-
-def get_profile() -> Profile | None:
-    return Profile.objects.first() #this ensures there will be only 1 profile
